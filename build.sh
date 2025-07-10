6cff7c839cef47eb1559c72c2cb77e18cc282d2f
Pedro Rosário <pedro.rosario@murabei.com>
2025-07-10 11:41:03 -0300

    using version file variable

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 VERSION_FILE="version"
 current_version=$(grep -E "^VERSION=" "$VERSION_FILE" | cut -d'=' -f2)
-IFS='.' read -r major minor patch <<< "$current_version"
+IFS='.' read -r major minor patch <<<"$current_version"
 patch=$((patch + 1))
 new_version="$major.$minor.$patch"
 sed -i "s/^VERSION=.*/VERSION=$new_version/" "$VERSION_FILE"
 echo "## Updated version to $new_version"
 
-source VERSION
-sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py > setup.py
+source $VERSION_FILE
+sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py >setup.py
 
 rm -R build/
 python3 setup.py build sdist bdist_wheel
